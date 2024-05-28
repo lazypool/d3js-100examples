a82de2b8e743691e175ad5ce03f3b39e9e1d146c
lazypool <3416382833@qq.com>
2024-05-28 15:27:12 +0800

    v0.0.1 - 项目架构雏形

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+export default function DashboardLayout({
+	children,
+}: {
+	children: React.ReactNode
+}) {
+	return (
+		<html lang="en">
+			<body>
+				<main>
+					{children}
+				</main>
+			</body>
+		</html>
+	)
+}
