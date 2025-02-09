38e2e99246c7942995ee8a03984b2188ec4cf0a9
lazypool <3416382833@qq.com>
2025-02-09 12:38:41 +0800

    v2.1.10 - FIFA 世界杯图表框架

---
+++ app/visual/fifa_worldcup/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+export default function FifaWorldCupPage() {
+	return (
+	<div className="subpage">
+			<h1>FIFA World Cup Statics</h1>
+			<div className="flex-row-container">
+				<div className="chart-container"></div>
+				<div className="infocard-container"></div>
+			</div>
+		</div>
+	)
+}
