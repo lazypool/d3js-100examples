192195d4efbe66bc4e84effd5276ae0c55b1ab88
lazypool <3416382833@qq.com>
2025-02-10 17:59:58 +0800

    v2.1.12 - FIFA 世界杯朴素信息卡片

--- app/visual/fifa_worldcup/page.tsx
+++ app/visual/fifa_worldcup/page.tsx
@@ -1,14 +1,24 @@
 "use client";
 
+import { useState } from "react";
 import FIFAWorldCupData from "./fifa_worldcup.json";
 
 export default function FifaWorldCupPage() {
+	const [selectedNode, setSelectedNode] = useState(FIFAWorldCupData[0])
+
 	return (
 	<div className="subpage">
 			<h1>FIFA World Cup Statics</h1>
 			<div className="flex-row-container">
 				<div className="chart-container"></div>
-				<div className="infocard-container"></div>
+				<div className="infocard-container">
+					<h3>{ selectedNode.edtion }</h3>
+					<table><tbody>
+					  <tr><td>Winner</td><td>{ selectedNode.winner }</td></tr>
+					  <tr><td>Goals</td><td>{ selectedNode.goals }</td></tr>
+					  <tr><td>Matches</td><td>{ selectedNode.matches }</td></tr>
+					</tbody></table> 
+				</div>
 			</div>
 		</div>
 	)
